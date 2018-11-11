4888eb6f6d954cef819bf57f11700d1e45f14a79
mikusHusbando <siryx97@gmail.com>
2018-11-11 16:01:37 +0100

    did some things broke it all

--- gtav-supply-bot.py
+++ gtav-supply-bot.py
@@ -1,95 +1,69 @@
-import discord
-import sys
 import time
-import threading
+
+import discord
+
 # read token from file so it is not in the repository
 with open('/root/discord-bot-secret') as f:
     discord_bot_secret = f.readline().strip()
 
 client = discord.Client()
-timerDict = {}
 
-business = [{'text':'bunker','ticTime':84},{'text':'coke','ticTime':72},{'text':'meth','ticTime':90},
-            {'text':'cash','ticTime':96},{'text':'weed','ticTime':120},{'text':'docs','ticTime':90}]
-ticSum = 100
+business = [{'type': 'bunker', 'ticTime': 84}, {'type': 'coke', 'ticTime': 72}, {'type': 'meth', 'ticTime': 90},
+            {'type': 'cash', 'ticTime': 96}, {'type': 'weed', 'ticTime': 120}, {'type': 'docs', 'ticTime': 90}]
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+bot_cmd_channel = discord.Object(id='494903538753863680')
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('!supplied:'):
-        await supplied(message)
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+amountTicks = 100
 
-client.run(discord_bot_secret)
 
-@client.event
-async def on_ready():
-    print('Bot is ready for use')
+async def check_progress_loop():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        #for b in business:
+        #    if b.type == parameter:
+        #        threading.Timer(time.time() + (ticSum * b.ticTime), timeIsUp, [message, parameter])
+        #        await client.send_message(message.channel,
+        #                                  'started timer for {}, finishes : {}.'.format(parameter,
+        #                                                                                timer[business.index(parameter)]))
+        await wrapper("waiting 5sec")
+        await asyncio.sleep(5)
 
-@client.event
-async def track(business):
-    client.send_message(message.channel, "tracking" + business)
-    return
 
-@client.event
-async def setup(business):
-    # fills the supplies of a business (done as part of the setup mission
-    return
 
-async def supplied(message):
-    parameter = message.content.split('!supplied:')[1].strip().lower()
-    author = message.author
-    for b in business:
-        if b.text == parameter:
-            threading.Timer(time.time() +(ticSum*b.ticTime), timeIsUp,[message,parameter])
-            await client.send_message(message.channel, 'started timer for {}, finishes : {}.'.format(parameter,timer[business.index(parameter)]))
-            return
-    # sets the supply to full in 10 minutes
-    await client.send_message(message.channel, 'wrong business!')
-    return
-async def timeIsUp(parameter):
-    await client.send_message(parameter[0].channel, 'timers up for {}!'.format(parameter[1]))
-    return
-@client.event
-async def sold(business):
-    # reset stock to zero
-    return
+async def wrapper(message_str):
+    await client.send_message(bot_cmd_channel, message_str)
 
-@client.event
-async def raided(business):
-    # reset stock to zero
-    # ask about supplies, gangster or police raid?
-    # maybe call this reset and let the user choose
-    return
+    @client.event
+    async def on_message(message):
 
-@client.event
-async def pause(business):
-    # pause the timer (when closing the game for example)
-    # maybe automatically call this if we pick up user leaving gta
-    return
+        # we want to only communicate in a dedicated channel to not spam the servers. hardcoded ID for now
+        if message.channel == bot_cmd_channel:
 
-@client.event
-async def resume(business):
-    #resume
-    return
+            if message.author == client.user:
+                return
 
-@client.event
-async def remind(minutes):
-    # set up a custom notification time
-    return
+            if message.content.startswith('!supplied'):
+                arguments = (extractArguments(message))
+                await supplied(arguments)
 
-@client.event
-async def status():
-    # print supply and stock info
-    return
+            if message.content.startswith('!hello'):
+                msg = 'Hello {0.author.mention}'.format(message)
+                await client.send_message(message.channel, msg)
+
+    def extractArguments(message):
+        message_parts = message.content.split(" ")
+        return [message.author, message_parts[0], message_parts[1], ]
+
+    async def supplied(arguments):
+        await wrapper(arguments)
+
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+
+    client.loop.create_task(check_progress_loop())
+
+    client.run(discord_bot_secret)

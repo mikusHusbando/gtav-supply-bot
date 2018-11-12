82c1b52a063c63406d1c4b1efc6d89ee95022743
Vincent Reckendrees <github@reckendrees.systems>
2018-11-12 13:32:12 +0100

    fix self

--- classes/timerdata.py
+++ classes/timerdata.py
@@ -7,7 +7,7 @@ class timerData:
 
     def add_timer(self, author_id, business_type, resupply_time):
         try:
-            author_data = user_supply_data[author_id]
+            author_data = self.user_supply_data[author_id]
         except KeyError:
             author_data = {}
         author_data[business_type] = resupply_time
@@ -20,8 +20,7 @@ class timerData:
         for author_id, author_data in self.user_supply_data.items():
             for business_type, resupply_time in author_data.items():
                 if current_time >= resupply_time:
-                    out.append({'type': business_type,
-                                'author_id': author_id})
+                    out.append({'type': business_type,'author_id': author_id})
         for o in out:  # reset elapsed timer that are returned. must be in a new loop, because looped arrays can not be modified
             self.user_supply_data[o['author_id']][o['type']] = 0
         return out
